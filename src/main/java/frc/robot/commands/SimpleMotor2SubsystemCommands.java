5e52df79e9bce559360597f192f94c4fa4585453
NoaMoshEliyahu <noamoshe.el@gmail.com>
2026-08-11 00:59:11 +0300

    MOTOR 2 commands

--- src/main/java/frc/robot/commands/SimpleMotor2SubsystemCommands.java
+++ src/main/java/frc/robot/commands/SimpleMotor2SubsystemCommands.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.units.VoltageUnit;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SimpleMotor2Subsystem;
@@ -11,10 +12,34 @@ public class SimpleMotor2SubsystemCommands extends Command {
     private double duration;
     private final Timer timer = new Timer();
 
-    public SimpleMotor2SubsystemCommands (SimpleMotor2Subsystem subsystem, double power, double duration){
+    public SimpleMotor2SubsystemCommands(SimpleMotor2Subsystem subsystem, double power, double duration) {
         this.subsystem = subsystem;
         this.power = power;
         this.duration = duration;
         addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        startTime = Timer.getFPGATimestamp();
+        timer.restart();
+        System.out.println("started At: " + startTime + " and operated for: " + duration + " the power is: " + power);
+    }
+
+    @Override
+    public void execute() {
+        subsystem.setPower(power);
+    }
+
+    @Override
+    public boolean isFinished(){
+        return (Timer.getFPGATimestamp() >= startTime + duration);
+    }
+
+    @Override
+    public void end (boolean ineteruption){
+        subsystem.stop();
+        System.out.println("Finished");
+        System.out.println("Ended at: " + Timer.getFPGATimestamp());
     }
 }
