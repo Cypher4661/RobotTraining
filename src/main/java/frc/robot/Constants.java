c59734b88fa4a2df768cd1a6311e4276f476c8b3
NoaMoshEliyahu <noamoshe.el@gmail.com>
2026-08-10 23:58:35 +0300

    STEER --> MOTOR

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,7 +20,7 @@ public final class Constants {
   }
   
 public static final class SimpleMotorSubsystemConstants {
-  public static final int STEER_ID = 1;
+  public static final int MOTOR_ID = 1;
   public static final String MotorCANbus = "rio";
 }
 
