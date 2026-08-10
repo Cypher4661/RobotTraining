4f86c9c374c0dd27bada6d0d205ff3179ea44a8d
NoaMoshEliyahu <noamoshe.el@gmail.com>
2026-08-10 23:09:41 +0300

    operative Autonomous Command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,9 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SimpleMotorSubsystemCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SimpleMotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -18,34 +20,56 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+
+  private final SimpleMotorSubsystem subsystem = new SimpleMotorSubsystem();
+
+public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+  }
+  private void configureDefaultCommands(){
+    subsystem.setDefaultCommand(new SimpleMotorSubsystemCommand(subsystem, 0, 0));
+  }
+public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return new SimpleMotorSubsystemCommand(subsystem, 0.7, 7);
+  }
+}
+
+
+
+
+
+
+
+/**
+ * 
+ *   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+ The container for the robot. Contains subsystems, OI devices, and commands.
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   */
+    Use this method to define your trigger->command mappings. Triggers can be created via the
+    {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+    predicate, or via the named factories in {@link
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
-
   }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+  
+    Use this to pass the autonomous command to the main {@link Robot} class.
+   
+   @return the command to run in autonomous
+   
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return Autos.exampleAuto(m_exampleSubsystem);
   }
-}
+  */
