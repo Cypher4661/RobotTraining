50406aabc73780951426527e323bb13b7a387009
NoaMoshEliyahu <noamoshe.el@gmail.com>
2026-08-11 00:59:56 +0300

    minor

--- src/main/java/frc/robot/commands/SimpleMotor2SubsystemCommands.java
+++ src/main/java/frc/robot/commands/SimpleMotor2SubsystemCommands.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.units.VoltageUnit;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SimpleMotor2Subsystem;
